c8e3a66e76c57c5e07c4d5584137a1b34ffe013a
Atay <atay.mamytov@gmail.com>
2019-09-21 07:38:34 +0000

    Added buttons, img folder, index.html. Changed: view for index

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -9,6 +9,6 @@ class Books(models.Model):
     name_author = models.CharField(max_length=3500, null=False, blank=False, verbose_name='Name')
     mail_author = models.CharField(max_length=3500, null=False, blank=False, verbose_name='Mail')
     entry = models.TextField(max_length=3500, null=False, blank=False, verbose_name='Entry')
-    created_at = models.DateField(auto_now_add=True, verbose_name='Date of create')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Date_of_create')
     changed_at = models.DateField(auto_now=True, verbose_name='Date of change')
     status = models.TextField(max_length=50, null=False, blank=False, choices=STATUS, default='active', verbose_name='Status')
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
 from webapp.models import Books
 
+
+def index_view(request, *args, **kwargs):
+    books = Books.objects.filter(status="Active").order_by("-changed_at")
+    return render(request, 'index.html', context={
+        'books': books
+    })
