133ebe3e09697502b57cbed1567ebb8f3feeda5e
Atay <atay.mamytov@gmail.com>
2019-09-21 06:30:16 +0000

    Made: model, migrations, fixtures

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+STATUS = [
+    ('Active', 'active'),
+    ('Blocked', 'blocked')
+]
+
+class Books(models.Model):
+    name_author = models.CharField(max_length=3500, null=False, blank=False, verbose_name='Name')
+    mail_author = models.CharField(max_length=3500, null=False, blank=False, verbose_name='Mail')
+    entry = models.TextField(max_length=3500, null=False, blank=False, verbose_name='Entry')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Date of create')
+    changed_at = models.DateField(auto_now=True, verbose_name='Date of change')
+    status = models.TextField(max_length=50, null=False, blank=False, choices=STATUS, default='active', verbose_name='Status')
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
+from webapp.models import Books
 
-# Create your views here.
